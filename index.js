131a1227aab21030f704a46c8b53645d5f90b93a
JustEnzoGangsta <157720508+JustEnzoGangsta@users.noreply.github.com>
2024-08-29 19:36:26 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ var btn04 = document.querySelector("#btn04");
 
 var calculer = document.querySelector("#calculer");
 var resultetxt = document.querySelector("#nombre");
+var texth2 = document.querySelector("h2");
 
 btn01.addEventListener("click", () => {
   resulte = resulte + 0.01;
@@ -54,5 +55,105 @@ calculer.addEventListener("click", () => {
   resultetxt.innerHTML = `
       <h2>${resulte}</h2>
   `;
-  resultetxt.style.transform = "rotate(5deg)";
+  resultetxt.style.transform = "rotate(2deg)";
+  resultetxt.style.background = "rgba(0, 0, 0, 0.596)";
+});
+
+// mouse over
+
+btn01.addEventListener("mouseover", () => {
+  btn01.style.transform = "rotate(2deg)";
+});
+
+btn01.addEventListener("mouseout", () => {
+  btn01.style.transform = "rotate(0deg)";
+});
+
+//
+
+btn02.addEventListener("mouseover", () => {
+  btn02.style.transform = "rotate(2deg)";
+});
+
+btn02.addEventListener("mouseout", () => {
+  btn02.style.transform = "rotate(0deg)";
+});
+
+//
+
+btn03.addEventListener("mouseover", () => {
+  btn03.style.transform = "rotate(2deg)";
+});
+
+btn03.addEventListener("mouseout", () => {
+  btn03.style.transform = "rotate(0deg)";
+});
+
+//
+
+btn04.addEventListener("mouseover", () => {
+  btn04.style.transform = "rotate(2deg)";
+});
+
+btn04.addEventListener("mouseout", () => {
+  btn04.style.transform = "rotate(0deg)";
+});
+
+//
+
+btn1.addEventListener("mouseover", () => {
+  btn1.style.transform = "rotate(2deg)";
+});
+
+btn1.addEventListener("mouseout", () => {
+  btn1.style.transform = "rotate(0deg)";
+});
+//
+
+btn2.addEventListener("mouseover", () => {
+  btn2.style.transform = "rotate(2deg)";
+});
+
+btn2.addEventListener("mouseout", () => {
+  btn2.style.transform = "rotate(0deg)";
+});
+
+//
+
+btn3.addEventListener("mouseover", () => {
+  btn3.style.transform = "rotate(2deg)";
+});
+
+btn3.addEventListener("mouseout", () => {
+  btn3.style.transform = "rotate(0deg)";
+});
+
+//
+
+btn4.addEventListener("mouseover", () => {
+  btn4.style.transform = "rotate(2deg)";
+});
+
+btn4.addEventListener("mouseout", () => {
+  btn4.style.transform = "rotate(0deg)";
+});
+
+//
+
+btn5.addEventListener("mouseover", () => {
+  btn5.style.transform = "rotate(2deg)";
+});
+
+//
+
+btn5.addEventListener("mouseout", () => {
+  btn5.style.transform = "rotate(0deg)";
+});
+
+texth2.addEventListener("mouseover", () => {
+  texth2.style.transform = "rotate(2deg)";
+});
+
+texth2.addEventListener("mouseout", () => {
+  texth2.style.transform = "rotate(0deg)";
 });
